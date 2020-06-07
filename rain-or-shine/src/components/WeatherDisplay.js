41dd1128188d9020fd01c5b61c75ac3632bd285f
Steven Frasica <sfrasica1@gmail.com>
2020-06-07 15:31:12 -0400

    added DailyCard component

--- rain-or-shine/src/components/WeatherDisplay.js
+++ rain-or-shine/src/components/WeatherDisplay.js
@@ -1,24 +1,40 @@
 import React, {useState, useEffect} from 'react'
 import apiConfig from '../apiKeys'
+import DailyCard from './DailyCard'
 
 const WeatherDisplay = () => {
 
+    const [completeData, setCompleteData] = useState([])
 
+    let [dailyData, setDailyData] = useState([])
 
     useEffect(() => {
-        const weatherURL =`http://api.openweathermap.org/data/2.5/weather?zip=10002,us&appid=${apiConfig.weatherKey}`
-
+        const weatherURL =`http://api.openweathermap.org/data/2.5/forecast?zip=11102&units=imperial&APPID=${apiConfig.weatherKey}`
+    
         fetch(weatherURL)
         .then(res => res.json())
-        .then(data => console.log("Data List Loaded", data.main))
+        .then(data => {
+            let dailyData = data.list.filter(reading => reading.dt_txt.includes("18:00:00"))
+            setDailyData(dailyData)
+      
+        })
     }
     )
+
+    const renderDailyCard = () => {
+        return dailyData.map((reading, index) => 
+        <DailyCard
+            reading = {reading}
+            key = {index}
+
+        />)
+    }
    
 
 
     return (
         <div>
-           <h1>I will display the weather</h1>
+            {renderDailyCard()}
         </div>
     )
 
