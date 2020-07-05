94bf793bdb87ca691998ffb72704fe548e383af9
Steven Frasica <sfrasica1@gmail.com>
2020-07-05 19:45:37 -0400

    incorporate useEffect

--- rain-or-shine/src/components/WeatherDisplay.js
+++ rain-or-shine/src/components/WeatherDisplay.js
@@ -41,7 +41,7 @@ const WeatherDisplay = () => {
     return (
         <div>
              <div className="container">
-      <h1 className="display-1 jumbotron">5-Day Forecast.</h1>
+      <h1 className="display-1 jumbotron">5-Day Forecast</h1>
       <h5 className="display-5 text-muted">New York, US</h5>
         <div className="row justify-content-center"></div>
             <DegreeToggle 
